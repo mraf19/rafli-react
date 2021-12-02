6f68be663231a7da6452e59639b5a4513b5f4faa
mraf19 <raflimr1902@gmail.com>
2021-12-02 10:43:57 +0700

    Adding Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import NewsHooks from "./NewsHooks";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import GlobalNav from "./GlobalNav";
+import Components from "./Components";
+import Page from "./Page";
+import Form from "./Form";
+import News from "./News";
+import NewsHooks from "./NewsHooks";
 
 function App() {
 	return (
-		<div className="App">
-			<NewsHooks />
-		</div>
+		<BrowserRouter>
+			<GlobalNav />
+			<Routes>
+				<Route path="/" exact element={<Components />} />
+				<Route path="/bio" element={<Page />} />
+				<Route path="/form" element={<Form />} />
+				<Route path="/news" element={<News />} />
+				<Route path="news-hooks/" element={<NewsHooks />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
